4323a9f850262a619cf27f988ce5c76fe3df0d11
Alex <123@gmail.com>
2026-02-16 21:21:18 +0300

    Implement unpack function.

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,38 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return "", nil
+	}
+
+	runes := []rune(s)
+	if unicode.IsDigit(runes[0]) {
+		return "", ErrInvalidString
+	}
+
+	for i := 1; i < len(runes); i++ {
+		if unicode.IsDigit(runes[i]) && unicode.IsDigit(runes[i-1]) {
+			return "", ErrInvalidString
+		}
+	}
+
+	var b strings.Builder
+	for i := 0; i < len(runes); i++ {
+		if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+			count := int(runes[i+1] - '0')
+			b.WriteString(strings.Repeat(string(runes[i]), count))
+			i++
+		} else {
+			b.WriteRune(runes[i])
+		}
+	}
+
+	return b.String(), nil
 }
