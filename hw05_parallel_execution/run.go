2f1b217bf3ffba13705e35de259c7acca7b2737d
Alex <123@gmail.com>
2026-03-28 22:21:51 +0300

    Realize parallel execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,54 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+func Run(tasks []Task, workersCount, maxErrorsCount int) error {
+	if workersCount <= 0 || maxErrorsCount <= 0 {
+		return ErrErrorsLimitExceeded
+	}
+
+	tasksCh := make(chan Task)
+	var wg sync.WaitGroup
+
+	var errCount int
+
+	var mtx sync.Mutex
+	wg.Add(workersCount)
+	for i := 0; i < workersCount; i++ {
+		go func() {
+			defer wg.Done()
+			for task := range tasksCh {
+				if err := task(); err != nil {
+					mtx.Lock()
+					errCount++
+					mtx.Unlock()
+				}
+			}
+		}()
+	}
+
+	for _, task := range tasks {
+		mtx.Lock()
+		if errCount >= maxErrorsCount {
+			mtx.Unlock()
+			break
+		}
+		mtx.Unlock()
+		tasksCh <- task
+	}
+
+	close(tasksCh)
+	wg.Wait()
+
+	if errCount >= maxErrorsCount {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,66 @@ func TestRun(t *testing.T) {
 		require.Equal(t, int32(tasksCount), runTasksCount, "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("tasks without errors: parallel workers (Eventually barrier)", func(t *testing.T) {
+		tasksCount := 50
+		begin := make(chan struct{})
+
+		var inFlight int32
+		var runTasksCount int32
+
+		tasks := make([]Task, 0, tasksCount)
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				atomic.AddInt32(&inFlight, 1)
+				<-begin
+				atomic.AddInt32(&inFlight, -1)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 5
+		maxErrorsCount := 1
+
+		errCh := make(chan error, 1)
+		go func() {
+			errCh <- Run(tasks, workersCount, maxErrorsCount)
+		}()
+
+		require.Eventually(t, func() bool {
+			return atomic.LoadInt32(&inFlight) >= int32(workersCount)
+		}, 5*time.Second, 10*time.Millisecond, workersCount)
+
+		close(begin)
+
+		require.NoError(t, <-errCh)
+		require.Equal(t, int32(tasksCount), runTasksCount, "not all tasks were completed")
+	})
+
+	t.Run("Task with zero WorkerCounts", func(t *testing.T) {
+		tasksCount := 10
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		for i := 0; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 0
+		maxErrorsCount := 1
+		var ErrTest = errors.New("errors limit exceeded")
+
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.EqualError(t, err, ErrTest.Error(), "actual err - %v", err)
+	})
 }
