b4cd76964c0c229eea59760acbcc080b96ca49ec
Alex <123@gmail.com>
2026-04-09 16:25:31 +0300

    hw07_file_copying: implement file copy utility with progress bar

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	pb "github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,49 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	src, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	info, err := src.Stat()
+	if err != nil {
+		return err
+	}
+
+	if !info.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	fileSize := info.Size()
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if _, err = src.Seek(offset, io.SeekStart); err != nil {
+		return err
+	}
+
+	bytesToCopy := fileSize - offset
+	if limit > 0 && limit < bytesToCopy {
+		bytesToCopy = limit
+	}
+
+	dst, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	bar := pb.Full.Start64(bytesToCopy)
+	defer bar.Finish()
+
+	reader := bar.NewProxyReader(src)
+	_, err = io.CopyN(dst, reader, bytesToCopy)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,72 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func readFile(t *testing.T, path string) []byte {
+	t.Helper()
+	data, err := os.ReadFile(path)
+	require.NoError(t, err)
+	return data
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	cases := []struct {
+		offset   int64
+		limit    int64
+		expected string
+	}{
+		{0, 0, "out_offset0_limit0.txt"},
+		{0, 10, "out_offset0_limit10.txt"},
+		{0, 1000, "out_offset0_limit1000.txt"},
+		{0, 10000, "out_offset0_limit10000.txt"},
+		{100, 1000, "out_offset100_limit1000.txt"},
+		{6000, 1000, "out_offset6000_limit1000.txt"},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.expected, func(t *testing.T) {
+			dst, err := os.CreateTemp("", "copy_test_*")
+			require.NoError(t, err)
+			dst.Close()
+			defer os.Remove(dst.Name())
+
+			err = Copy(filepath.Join("testdata", "input.txt"), dst.Name(), tc.offset, tc.limit)
+			require.NoError(t, err)
+
+			got := readFile(t, dst.Name())
+			want := readFile(t, filepath.Join("testdata", tc.expected))
+			require.Equal(t, want, got)
+		})
+	}
+}
+
+func TestCopyOffsetExceedsFileSize(t *testing.T) {
+	dst, err := os.CreateTemp("", "copy_test_*")
+	require.NoError(t, err)
+	dst.Close()
+	defer os.Remove(dst.Name())
+
+	err = Copy(filepath.Join("testdata", "input.txt"), dst.Name(), 1<<32, 0)
+	require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+}
+
+func TestCopyUnsupportedFile(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("no device files on Windows")
+	}
+
+	dst, err := os.CreateTemp("", "copy_test_*")
+	require.NoError(t, err)
+	dst.Close()
+	defer os.Remove(dst.Name())
+
+	err = Copy("/dev/urandom", dst.Name(), 0, 0)
+	require.ErrorIs(t, err, ErrUnsupportedFile)
 }
